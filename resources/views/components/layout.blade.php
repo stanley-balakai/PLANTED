9b012d28d1d62790e303a0d654165ef537c29ec1
David Benner <davidbenner@davids-mbp.lan>
2023-04-25 13:28:15 -0400

    added posts view and set up basic styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,8 @@
 <title>Planted</title>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
@@ -21,9 +23,7 @@
     <section class="px-6 py-8">
         <nav class="md:flex md:justify-between md:items-center">
             <div>
-                <a href="/">
-                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
-                </a>
+                
             </div>
 
             <div class="mt-8 md:mt-0">
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,51 +1,49 @@
+<!-- posts.blade.php -->
+
 <x-layout>
+    <head>
+        <style>
+            body {
+                background-color: #004d40;
+                color: #b2dfdb;
+            }
+
+            .post-card {
+                background-color: #00796b;
+                border-radius: 10px;
+                padding: 20px;
+                transition: transform 0.3s ease;
+            }
+
+            .post-card:hover {
+                transform: translateY(-5px);
+            }
+
+            .accent {
+                color: #ff8f00;
+            }
+        </style>
+    </head>
     <section class="px-6 py-8">
         <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
-            <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
-                <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
-                    <img src="/images/illustration-1.png" alt="" class="rounded-xl">
-
-                    <p class="mt-4 block text-gray-400 text-xs">
-                        Published <time>{{ $post->created_at->diffForHumans() }}</time>
-                    </p>
-
-                    <div class="flex items-center lg:justify-center text-sm mt-4">
-                        <img src="/images/lary-avatar.svg" alt="Lary avatar">
-                        <div class="ml-3 text-left">
-                            <h5 class="font-bold">{{ $post->author->name }}</h5>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-span-8">
-                    <div class="hidden lg:flex justify-between mb-6">
-                        <a href="/"
-                           class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
-                            <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
-                                <g fill="none" fill-rule="evenodd">
-                                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                                    </path>
-                                    <path class="fill-current"
-                                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                                    </path>
-                                </g>
-                            </svg>
-
-                            Back to Posts
-                        </a>
-
-                        <div class="space-x-2">
-                            <x-category-button :category="$post->category" />
-                        </div>
-                    </div>
-
-                    <h1 class="font-bold text-3xl lg:text-4xl mb-10">
-                        {{ $post->title }}
-                    </h1>
-
-                    <div class="space-y-4 lg:text-lg leading-loose">{{ $post->body }}</div>
-                </div>
-            </article>
+            <h1 class="text-center text-4xl font-bold accent mb-10">Planted</h1>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <article class="post-card">
+                    <h2 class="text-2xl font-bold mb-2">Post 1 Title</h2>
+                    <p class="mb-4">This is a summary or excerpt of post 1. You can add more content here.</p>
+                    <a href="#" class="accent">Read More &rarr;</a>
+                </article>
+                <article class="post-card">
+                    <h2 class="text-2xl font-bold mb-2">Post 2 Title</h2>
+                    <p class="mb-4">This is a summary or excerpt of post 2. You can add more content here.</p>
+                    <a href="#" class="accent">Read More &rarr;</a>
+                </article>
+                <article class="post-card">
+                    <h2 class="text-2xl font-bold mb-2">Post 3 Title</h2>
+                    <p class="mb-4">This is a summary or excerpt of post 3. You can add more content here.</p>
+                    <a href="#" class="accent">Read More &rarr;</a>
+                </article>
+            </div>
         </main>
     </section>
 </x-layout>
