49470a2391cfc6dfefd28359dbf5999d70e4d018
David Benner <davidbenner@davids-mbp.lan>
2023-04-26 15:44:18 -0400

    Navbar search bar created

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -71,33 +71,62 @@
     @endguest
 </div>
 
+
+
+
+
         
-                <!-- Right section -->
-                <div class="flex-grow md:flex-grow-0 text-right">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-white hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-white hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-        
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-white hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
+<!-- Right section -->
+<div class="hidden sm:flex sm:items-center sm:ml-6">
+    @guest
+    <!-- Login/Register prompt -->
+    <div>
+        <a href="{{ route('login') }}" class="text-sm text-white-700 underline">Log in</a>
+        <span class="text-sm text-gray-700">or</span>
+        <a href="{{ route('register') }}" class="ml-2 text-sm text-white-700 underline">Register</a>
+    </div>
+    @else
+    <!-- Logged-in user dropdown -->
+    <x-dropdown align="right" width="48">
+        <x-slot name="trigger">
+            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                <div>Welcome, {{ Auth::user()->name }}</div>
+
+                <div class="ml-1">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    </svg>
                 </div>
+            </button>
+        </x-slot>
+
+        <x-slot name="content">
+            <x-dropdown-link :href="route('profile.edit')">
+                {{ __('Profile') }}
+            </x-dropdown-link>
+
+            <!-- Authentication -->
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <x-dropdown-link :href="route('logout')"
+                    onclick="event.preventDefault();
+                    this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-dropdown-link>
+            </form>
+        </x-slot>
+    </x-dropdown>
+    @endguest
+</div>
+
+
+
+
+                
             </div>
         </nav>
         
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -136,8 +165,3 @@
 </footer>
 </section>
 </body>
-
-
-
-
-
